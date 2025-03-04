73e142e2f86c7e7364bef1f20df6a3c8ba33aade
linh0 <linh0@nhlinh>
2025-03-04 20:38:50 +0700

    linh

---
+++ src/main/java/com/mycompany/baithuchanhso3/BaiThucHanhSo3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.baithuchanhso3;
+
+/**
+ *
+ * @author linh0
+ */
+public class BaiThucHanhSo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
